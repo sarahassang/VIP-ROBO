d715efa026c907f61a486bc1a7b1bb2225836481
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-05 02:09:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,11 +7,11 @@ bot = Client(
     "mo",
     api_id=21627756,
     api_hash="fe77fbf0cae9f7f5ece37659e2466cf1",
-    bot_token="6420574235:AAEO42nR-NBTKXo5vrcu9Q_ptNAEU1hM2yQ",
+    bot_token="6445765629:AAGYzIlQDSfpBcqlklDJ_WU_tNrZif_Pcrs",
     plugins=dict(root="MZombie")
     )
 
-DEVS = ["UP_UO"]
+DEVS = ["lll_lll89"]
 
 bot_id = bot.bot_token.split(":")[0]
 
