66550d47b1da827940c68f25f7188586b88429af
sarahassang <145998215+sarahassang@users.noreply.github.com>
2024-01-01 04:06:41 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,11 +7,11 @@ bot = Client(
     "mo",
     api_id=21627756,
     api_hash="fe77fbf0cae9f7f5ece37659e2466cf1",
-    bot_token="6329696412:AAGWAmtQqtcWcZgFxoJ-n5PqSnOOrX9Al7w",
+    bot_token="6554844168:AAFsaUSbj35ftkVXbZz-OHEO95BVbDsU_fc",
     plugins=dict(root="MZombie")
     )
 
-DEVS = ["lllliioooll"]
+DEVS = ["JKI_ll"]
 
 bot_id = bot.bot_token.split(":")[0]
 
